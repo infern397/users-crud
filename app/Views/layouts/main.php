0d554b43f7d95bb7ea31887e7bcfd10805a8bb66
infern <semenmochalov55@gmail.com>
2025-09-07 21:42:58 +0700

    add Bootstrap integration with layout and users table

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -9,12 +9,24 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?? 'App' ?></title>
+    <link href="/assets/bootstrap/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
-<div>
+<div class="container">
+    <header class="py-2 d-flex justify-content-between align-items-center">
+        <div class="col">
+            <h1>Users CRUD</h1>
+        </div>
+        <div class="col text-end">
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+        </div>
+    </header>
+
     <?= $content ?>
 </div>
 
+<script src="/assets/bootstrap/bootstrap.min.js"></script>
 </body>
 </html>
